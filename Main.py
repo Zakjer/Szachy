f8441d61c039827b8c0e005ba56348eca59a34f4
Zakjer <340631@uwr.edu.pl>
2023-11-01 23:08:51 +0100

    Final changes

--- Main.py
+++ Main.py
@@ -23,7 +23,6 @@ def load_images():
 
 def main():
     """Funkcja główna pobiera input od użytkownika oraz na bieżąco aktualizuje wygląd szachownicy"""
-    chosen_color = input('Pick board color (recommended color is gray): ')
     surface = p.display.set_mode((WIDTH, HEIGHT))
     clock = p.time.Clock()
     p.display.set_caption("Chess")
@@ -96,12 +95,12 @@ def main():
 
         if move_made:
             if animate:
-                animate_move(gs.move_log[-1], surface, gs.board, clock, chosen_color)
+                animate_move(gs.move_log[-1], surface, gs.board, clock)
             valid_moves = gs.get_valid_moves()
             move_made = False
             animate = False 
 
-        draw_game_state(surface, gs, valid_moves, sq_selected, chosen_color)
+        draw_game_state(surface, gs, valid_moves, sq_selected)
         #Rysowanie komunikatów po zakończeniu gry
         if gs.checkmate:
             gameover = True
@@ -116,16 +115,16 @@ def main():
         clock.tick(FPS)
         p.display.flip()
 
-def draw_game_state(surface, gs, valid_moves, sq_selected, chosen_color):
+def draw_game_state(surface, gs, valid_moves, sq_selected):
     """Funkcja odpowiedzialna za wyświetlenie obecnego stanu gry"""
-    draw_board(surface, chosen_color)
+    draw_board(surface)
     highlight_squares(surface, gs, valid_moves, sq_selected)
     draw_pieces(surface, gs.board)
 
-def draw_board(surface, chosen_color):
+def draw_board(surface):
     """Funkcja rysująca szachownicę"""
     global colors
-    colors = [p.Color("white"), p.Color(chosen_color)]
+    colors = [p.Color("white"), p.Color('gray')]
     for row in range(DIMENSION):
         for column in range(DIMENSION):
             color = colors[((row+column) % 2)] #Rysowanie białych oraz szarych kwadratów
@@ -156,7 +155,7 @@ def highlight_squares(surface, gs, valid_moves, sq_selected):
                 if move.start_row == row and move.start_column == column:
                     surface.blit(s, (move.end_column * SQ_SIZE, move.end_row * SQ_SIZE))
 
-def animate_move(move, surface, board, clock, chosen_color):
+def animate_move(move, surface, board, clock):
     """Funkcja animująca ruch figury"""
     global colors 
     dR = move.end_row - move.start_row
@@ -166,7 +165,7 @@ def animate_move(move, surface, board, clock, chosen_color):
     for frame in range(frame_count + 1):
         row = move.start_row + dR*frame/frame_count
         column = move.start_column + dC*frame/frame_count
-        draw_board(surface, chosen_color)
+        draw_board(surface)
         draw_pieces(surface, board)
         #Usunięcie figury z położenia końcowego
         color = colors[(move.end_row + move.end_column) % 2]
@@ -196,9 +195,7 @@ def draw_text(surface, text):
     surface.blit(text_object, text_location.move(2, 2))
     
 if __name__ == "__main__":
-    try:
         main()
-    except ValueError:
-        print('Chosen color is wrong')
+   
 
     
